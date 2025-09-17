3ba1d7a4c9440c82a8395b13bf712e9287885eee
CHETHAN B <chethan140205@gmail.com>
2025-09-17 12:16:08 +0530

    hi

--- src/main/java/com/mtd/ecom_server/controllers/ProductController.java
+++ src/main/java/com/mtd/ecom_server/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package com.mtd.ecom_server.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +20,11 @@ import com.mtd.ecom_server.repos.ProductRepo;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+	private static final Logger Log=LoggerFactory.getLogger(ProductController.class);
 	@Autowired ProductRepo productRepo;
 	@GetMapping("/all")
 	public List<Product> getAllProducts(){
+		Log.info("Fetching Products");
 		return productRepo.findAll();
 	}
 	@PostMapping("/add")
@@ -27,16 +32,19 @@ public class ProductController {
 		return productRepo.save(newproduct);
 	}		
 	@DeleteMapping("/product/{id}")
+	
+	
 	public String deleteProduct (@PathVariable String id) {
-		Product findproduct =productRepo.findById(id).get();
-		if(findproduct !=null) {
-			productRepo.deleteById(id);
-			return "Product Deleted" +findproduct.getName();
+		Optional<Product> findproduct =productRepo.findById(id);
+		if(findproduct.isEmpty()) {
+			Log.error("failed to delete product"+id);
+			return "Product Deleted";
 		}
-		else {
-			return "Failed to delete product";
+		productRepo.deleteById(id);
+		Log.error("deleting the product by id"+id);
+		return "deleted product";
 		}
-	}
+	
 	
 	@PutMapping("/product/edit/{id}")
 	public Product editProduct(@PathVariable String id, @RequestBody Product newProduct) {
@@ -48,6 +56,7 @@ public class ProductController {
 	    findproduct.setTags(newProduct.getTags());
 	    findproduct.setPrice(newProduct.getPrice());
 	    findproduct.setStock(newProduct.getStock());
+		Log.info("editing Products");
 
 	    return productRepo.save(findproduct);
 	}
